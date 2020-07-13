f89c0e716e0cc45d4d9550f2c84c2ff094545aae
Jeff Jenkins <jjenkins120@gmail.com>
2020-07-13 20:06:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+def key_for_min_value(hash)
+    comparative_value = 1000
+    hash.each do |k,v|
+        if v <= comparative_value
+            v == comparative_value
+        end
+    return k
+    end
+end
 
+puts key_for_min_value(ikea)
 
 
 
